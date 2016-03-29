14136e0798eb3d9f75d37c4d98cd80e74fe23dfa
tr1ck <meyers.ricky@gmail.com>
2016-03-29 02:33:58 +0000

    Setup pipeline for user page

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Nomster::Application.routes.draw do
   resources :places do
     resources :photos, only: :create
   end
+
+  resources :users, only: :show
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
